78aee6ecc9d3b53858039c6d0db404235d0f3235
Hyrum <hyrum.coleman@gmail.com>
2025-03-21 14:04:44 -0600

    split uart code up to make it less annoying to dev with features
    Repeated, general code is moved up into its own function, and called from the feature specific functions. That way we don't have to do all our edits twice

--- src/bin/async_main/uart.rs
+++ src/bin/async_main/uart.rs
@@ -5,6 +5,7 @@ use core::ffi::CStr;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 #[cfg(feature = "wifi")]
 use embassy_sync::signal::Signal;
+use embassy_sync::watch::DynSender;
 use embassy_time::Timer;
 use esp_hal::gpio::Output;
 use esp_hal::uart::UartRx;
@@ -12,67 +13,75 @@ use esp_hal::Async;
 use heapless::Vec;
 use log::{error, info, warn};
 
-/// Reads UART channel, then attemps to convert recieved bytes into an f32
-/// TODO: UART is probably sending character bytes, which won't cleanly convert to an f32, attempt conversion to char array, then f32
-#[cfg(not(feature = "wifi"))]
-#[embassy_executor::task]
-pub async fn read_uart(mut uart: UartRx<'static, Async>, mut builtin_led: Output<'static>) {
+/// Handles shared UART logic for parsing SensorData and triggering alerts.
+async fn process_uart_read(
+    uart: &mut UartRx<'static, Async>,
+    builtin_led: &mut Output<'static>,
+) -> Option<SensorData> {
     let mut uart_buffer: [u8; 128] = [0u8; 128];
-    let sender = WATCH.dyn_sender();
 
-    loop {
-        let Ok(_len) = uart.read_async(&mut uart_buffer).await else {
-            error!("Error reading UART");
-            continue;
-        };
+    let Ok(_len) = uart.read_async(&mut uart_buffer).await else {
+        error!("Error reading UART");
+        return None;
+    };
 
-        let sensor_ascii = match CStr::from_bytes_until_nul(&uart_buffer) {
-            Ok(valid_ascii) => valid_ascii,
-            Err(e) => {
-                warn!("UART Buffer: {:?}", uart_buffer);
-                error!("{}", e);
-                continue;
-            }
+    let sensor_ascii = match CStr::from_bytes_until_nul(&uart_buffer) {
+        Ok(valid_ascii) => valid_ascii,
+        Err(e) => {
+            warn!("UART Buffer: {:?}", uart_buffer);
+            error!("{}", e);
+            return None;
         }
-        .to_str()
-        .unwrap();
+    }
+    .to_str()
+    .unwrap_or_default();
 
-        let variables: Vec<f32, 2> = sensor_ascii
-            .trim()
-            .split(',')
-            .flat_map(|var| var.parse::<f32>())
-            .collect();
+    let variables: Vec<f32, 2> = sensor_ascii
+        .trim()
+        .split(',')
+        .flat_map(|var| var.parse::<f32>())
+        .collect();
 
-        if variables.len() != 2 {
-            error!("SHit's fucked: {:?}", variables);
-            builtin_led.set_high();
-            continue;
-        } else {
-            builtin_led.set_low();
+    if variables.len() != 2 {
+        error!("Failed to parse two floats: {:?}", variables);
+        builtin_led.set_high();
+        return None;
+    } else {
+        builtin_led.set_low();
+    }
+
+    Some(SensorData::new(variables[1], variables[0]))
+}
+
+async fn send_alert(sensor_data: SensorData, sender: &DynSender<'static, DetectionStatus>) {
+    match sensor_data.velocity.total_cmp(&VELOCITY_THRESHOLD) {
+        core::cmp::Ordering::Less => {
+            sender.send(DetectionStatus::Clear);
         }
-
-        uart_buffer = [0u8; 128];
-
-        let sensor_data = SensorData::new(variables[1], variables[0]);
-
-        info!(
-            "Time: {} -- Velocity Reading: {}",
-            sensor_data.time, sensor_data.velocity
-        );
-
-        match sensor_data.velocity >= VELOCITY_THRESHOLD {
-            true => {
-                sender.send(DetectionStatus::ObjectDetected);
-                Timer::after_millis(ALERT_TIME_MILLISECONDS.into()).await;
-            }
-            false => {
-                sender.send(DetectionStatus::Clear);
-            }
-        };
+        _ => {
+            sender.send(DetectionStatus::ObjectDetected);
+            Timer::after_millis(ALERT_TIME_MILLISECONDS.into()).await;
+        }
     }
 }
 
-// wifi version of read_uart
+#[cfg(not(feature = "wifi"))]
+#[embassy_executor::task]
+pub async fn read_uart(mut uart: UartRx<'static, Async>, mut builtin_led: Output<'static>) {
+    let sender = WATCH.dyn_sender();
+
+    loop {
+        if let Some(sensor_data) = process_uart_read(&mut uart, &mut builtin_led).await {
+            info!(
+                "Time: {} -- Velocity Reading: {}",
+                sensor_data.time, sensor_data.velocity
+            );
+
+            send_alert(sensor_data, &sender).await;
+        }
+    }
+}
+
 #[cfg(feature = "wifi")]
 #[embassy_executor::task]
 pub async fn read_uart(
@@ -80,64 +89,18 @@ pub async fn read_uart(
     signals: &'static Signal<CriticalSectionRawMutex, SensorData>,
     mut builtin_led: Output<'static>,
 ) {
-    let mut uart_buffer: [u8; 128] = [0u8; 128];
     let sender = WATCH.dyn_sender();
 
     loop {
-        let Ok(_len) = uart.read_async(&mut uart_buffer).await else {
-            error!("Error reading UART");
-            continue;
-        };
+        if let Some(sensor_data) = process_uart_read(&mut uart, &mut builtin_led).await {
+            info!(
+                "Time: {} -- Velocity Reading: {}",
+                sensor_data.time, sensor_data.velocity
+            );
 
-        let sensor_ascii = match CStr::from_bytes_until_nul(&uart_buffer) {
-            Ok(valid_ascii) => valid_ascii,
-            Err(e) => {
-                warn!("UART Buffer: {:?}", uart_buffer);
-                error!("{}", e);
-                continue;
-            }
+            signals.signal(sensor_data.clone());
+
+            send_alert(sensor_data, &sender).await;
         }
-        .to_str()
-        .unwrap();
-
-        let variables: Vec<f32, 2> = sensor_ascii
-            .trim()
-            .split(',')
-            .flat_map(|var| var.parse::<f32>())
-            .collect();
-
-        if variables.len() != 2 {
-            error!("SHit's fucked: {:?}", variables);
-            builtin_led.set_high();
-            continue;
-        } else {
-            builtin_led.set_low();
-        }
-
-        uart_buffer = [0u8; 128];
-
-        let sensor_data = SensorData::new(variables[1], variables[0]);
-
-        info!(
-            "Time: {} -- Velocity Reading: {}",
-            sensor_data.time, sensor_data.velocity
-        );
-
-        signals.signal(sensor_data.clone());
-
-        match sensor_data.velocity.total_cmp(&VELOCITY_THRESHOLD) {
-            core::cmp::Ordering::Less => {
-                sender.send(DetectionStatus::Clear);
-            }
-            core::cmp::Ordering::Equal => {
-                sender.send(DetectionStatus::ObjectDetected);
-                Timer::after_millis(ALERT_TIME_MILLISECONDS.into()).await;
-            }
-            core::cmp::Ordering::Greater => {
-                sender.send(DetectionStatus::ObjectDetected);
-                Timer::after_millis(ALERT_TIME_MILLISECONDS.into()).await;
-            }
-        };
-        // info!("Velocity Reading: {:?}", velocity_reading);
     }
 }
